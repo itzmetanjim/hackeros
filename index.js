6636c161c45dd719a04f552bcd8b40466bc83d2c
Tanjim Kamal <tanjimkamal1@gmail.com>
2026-03-23 22:50:41 +0600

    maybe fix some stuff

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const bframe=document.getElementById("backgroundframe");
+
 bframe.addEventListener("load",()=>{
     const doc=bframe.contentDocument||bframe.contentWindow.document;
     try{
         doc.getElementById("ui").style.display="none";
     }catch(e){console.error(e)}
 
+})
+bframe.width=window.innerWidth;
+bframe.height=window.innerHeight;
+
+window.addEventListener("resize",()=>{
+    bframe.width=window.innerWidth;
+    bframe.height=window.innerHeight;
 })
